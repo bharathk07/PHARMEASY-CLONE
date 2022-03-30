a8fc20ac0f6b23656b8f59e65441e223b79b033e
jayasmita12 <jayasmitasahu487448@gmail.com>
2022-03-30 22:11:56 +0530

    add healthcare devices

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,12 +2,12 @@ const  connect  = require("./configs/db")
 const app = require("./backend")
 
 
-app.listen(6000,async()=>{
+app.listen(5300,async()=>{
     try{
         await connect()
-        console.log("Connect to Port 6000...")
+        console.log("Connect to Port 5300...")
     }
     catch(err){
-
+        console.log(err.message)
     }
 })
